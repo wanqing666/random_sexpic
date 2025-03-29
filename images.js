292af2024af3ca5bc769dec3710801c6ca131761
wanqing666 <storwq666@gmail.com>
2025-03-29 17:33:10 +0800

    更新 images.js

--- images.js
+++
@@ -1,9 +0,0 @@
-// 图片数据库
-const imageData = [
-    {"pid":"81449047","url":"https://i.pixiv.re/img-original/img/2020/05/10/19/20/55/81449047_p0.jpg"},
-    {"pid":"98207361","url":"https://i.pixiv.re/img-original/img/2022/05/08/20/26/50/98207361_p1.jpg"},
-    {"pid":"87010421","url":"https://i.pixiv.re/img-original/img/2021/01/12/22/00/07/87010421_p3.png"},
-    {"pid":"55837138","url":"https://i.pixiv.re/img-original/img/2016/03/16/02/23/31/55837138_p0.png"},
-    {"pid":"85195103","url":"https://i.pixiv.re/img-original/img/2020/10/24/00/00/00/85195103_p0.png"}
-    // 你可以在这里继续添加更多图片数据...
-];
